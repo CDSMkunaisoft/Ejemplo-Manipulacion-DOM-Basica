4bc30088bbc2b03b37f8bae268d64caee9c4d3ed
beautyNy <kunaisoftcesarsequeda@gmail.com>
2024-01-18 14:28:40 -0400

    cambio prueba 1

---
+++ fase 1/practica-switch_por_if.js
@@ -0,0 +1,15 @@
+//let  tipoDeSuscripcion =[free,basic,expert,expertDuo]
+if (tipoDeSuscripcion == "free"){
+        console.log("solo usar los cursos gratis")
+}
+else if(tipoDeSuscripcion=="basic"){
+    console.log ("puedes acceder a todos los cursos y tambien puedes hacer mas cursos")
+}
+else if(tipoDeSuscripcion=="expert"){
+    console.log("usar todos los cursos y crear cursos")
+}
+else if (tipoDeSuscripcion == "expertDuo"){
+    console.log("puedes hacer lo mismo que la cuenta expert, pero con otro amigo")
+}
+
+const tipoDeSuscripcion = "free"
