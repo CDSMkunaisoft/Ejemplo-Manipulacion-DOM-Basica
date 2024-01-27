55b77cd6347b6cc6336df0fee6374aae5b445254
Cesar-Sequeda <cesardasm@gmail.com>
2024-01-27 12:48:08 -0400

    arreglo

--- fase-3/script.js
+++ fase-3/script.js
@@ -1,15 +1,15 @@
-const h1 = document.querySelector("h1")
-const form = document.querySelector("#form")
-const  input1 = document.querySelector("#calculo1")
-const  input2 = document.querySelector("#calculo2")
-const btn = document.querySelector("#btnCalcular")
-const pResult = document.querySelector("#result")
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-form.addEventListener('submit', sumarInputValues)
-function sumarInputValues (event) {
-    event.preventDefault()
-    const  sumaInputs= input1.value + input2.value;
-    pResult.innerText = "Resultado:"+ sumaInputs
+form.addEventListener("submit", sumarInputValues);
+function sumarInputValues(event) {
+  event.preventDefault();
+  const sumaInputs = input1.value + input2.value;
+  pResult.innerText = "Resultado:" + sumaInputs;
 }
 //manera de escuchar eventos de los usuarios desde js
 //prueba1
